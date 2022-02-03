7816dd46c62078732441d43f7f70fbca8dee23d4
Segura <segura-deluna@mail.ru>
2022-02-03 13:49:26 +0500

    Added submenu in the burger-menu

--- dist/js/dropdown.js
+++ dist/js/dropdown.js
@@ -4,19 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
   function openDropdown() {
     const dropdown = document.querySelectorAll('.dropdown');
 
-    for (item of dropdown) {
-      item.addEventListener('click', function () {
-        if (this.classList.contains('active')) {
-          this.classList.remove('active');
-        } else {
-          for (el of dropdown) {
-            el.classList.remove('active');
-          }
-          this.classList.add('active');
-          console.log(this);
-        }
-      });
-    }
+    dropdown.onclick = function () {
+      this.classList.toggle('active');
+    };
   }
   openDropdown();
 });
